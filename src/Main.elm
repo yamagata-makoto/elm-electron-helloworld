6ae137a93c0c6a46310f66e909e7bb506b7354f9
makoto yamagata <yamagata@clouds-inc.jp>
2019-07-28 12:58:11 +0900

    change message.

--- src/Main.elm
+++ src/Main.elm
@@ -9,5 +9,9 @@ view model =
   Html.text model
 
 main =
-    Browser.sandbox { init = "Hello, World from Elm!", update = update, view = view }
+    Browser.sandbox
+        { init = "Hello, World from Elm!"
+        , update = update
+        , view = view
+        }
 
