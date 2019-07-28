a1ec92f78c79e0dce46fb7b654216fd7201514bc
makoto yamagata <yamagata@clouds-inc.jp>
2019-07-28 12:39:53 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
+app.on('ready', function() {
+    var server = require('child_process').spawn('python',['./server.py']);
+    var requestPromise = require('request-promise');
+    var indexURL = 'http://localhost:5000/';
+
+    var startUp = function() {
+        requestPromise(indexURL)
+            .then(function(htmlString) {
+                console.log('server started');
+
+                let mainWindow = new BrowserWindow({ width: 400, height: 300 });
+                mainWindow.loadURL(indexURL)
+
+                mainWindow.on('closed', function() {
+                    server.kill('SIGINT');
+                });
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    };
+
+    startUp();
+});
+
---
+++ src/Main.elm
@@ -0,0 +1,13 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+
+update msg _ = msg
+
+view model =
+  Html.text model
+
+main =
+    Browser.sandbox { init = "Hello, World from Elm!", update = update, view = view }
+
